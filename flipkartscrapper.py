3734e3e566ca24fb489307b304dd946f04e8b071
shahidfortheye <109901959+shahidfortheye@users.noreply.github.com>
2024-05-30 02:11:18 +0530

    Update flipkartscrapper.py

--- flipkartscrapper.py
+++ flipkartscrapper.py
@@ -84,6 +84,13 @@ class FlipkartScrapper:
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--window-size=1920,1080")
+
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36")
+        options.add_argument("--ignore-certificate-errors")
+
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
         # if self.proxy:
         #     proxy_host = self.proxy[0]
         #     proxy_port = self.proxy[1]
